4d97ba8ca8a4c09a8c841a2be87c0cdfcd43cc60
Facundo Palombo <facundo.palombo@gmail.com>
2019-10-15 11:40:37 -0300

    Functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export const App = () => {
   return(
     <Layout>
       <h1>Hello world!</h1>
+      <h1 className="title-grey">Grey</h1>
+      <h2 className="title-dark">Grey-dark</h2>
+      <h3 className="title-darker">Grey-darker</h3>
+      <h2 className="title-light">Grey-light</h2>
+      <h3 className="title-lighter">Grey-lighter</h3>
+      <h4 className="title-substract">Sample</h4>
+      <h4 className="title-add">Sample</h4>
+      <h4 className="title-color">Sample</h4>
+      <h4 className="title-color-desaturate">Sample</h4>
+      <h4 className="title-color-spin">Sample</h4>
     </Layout>
   )
 }
